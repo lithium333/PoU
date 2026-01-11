9e1f1c75d54b3e29c0ab8e8c2900bbdbfc87fa8b
lithium333 <119898873+lithium333@users.noreply.github.com>
2026-01-11 12:01:51 +0100

    LowLevel timers management

--- fw_PoU.ino
+++ fw_PoU.ino
@@ -31,6 +31,7 @@
 //2 wait data, command or end of stream
 //3 wait 2nd part of 8 bit data packet
 
+#include <avr/wdt.h>
 #include <EEPROM.h>
 
 volatile uint8_t fsm_state=0;
@@ -42,6 +43,7 @@ uint8_t reg_port0=0;
 uint8_t port_cnt=0;
 
 void setup() {
+  wdt_disable();
   Serial.begin(115200);
   pinMode(PWMPORT_0,OUTPUT);
   pinMode(PWMPORT_1,OUTPUT);
@@ -49,12 +51,20 @@ void setup() {
   pinMode(PWMPORT_3,OUTPUT);
   pinMode(PWMPORT_4,OUTPUT);
   pinMode(PWMPORT_5,OUTPUT);
-  analogWrite(PWMPORT_0,0);
-  analogWrite(PWMPORT_1,0);
-  analogWrite(PWMPORT_2,0);
-  analogWrite(PWMPORT_3,0);
-  analogWrite(PWMPORT_4,0);
-  analogWrite(PWMPORT_5,0);
+  // TIMERS SETUP
+  TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(WGM00);
+  TCCR0B = _BV(CS00);
+  OCR0A = 0;
+  OCR0B = 0;
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM10);
+  TCCR1B = _BV(CS10);
+  OCR1A = 0;
+  OCR1B = 0;
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
+  TCCR2B = _BV(CS20);
+  OCR2A = 0;
+  OCR2B = 0;
+  // read own addr
   own_addr_base4 = EEPROM.read(ROM_ADDR4);
   own_addr_base8 = EEPROM.read(ROM_ADDR8);
 }
@@ -117,24 +127,24 @@ void loop() {
             fsm_state=4;
           } else if (d_reg==KCODE_RES_UPD) { // update
             // update reg to port procedure
-            analogWrite(PWMPORT_0,data_read[0]);
-            analogWrite(PWMPORT_1,data_read[1]);
-            analogWrite(PWMPORT_2,data_read[2]);
-            analogWrite(PWMPORT_3,data_read[3]);
-            analogWrite(PWMPORT_4,data_read[4]);
-            analogWrite(PWMPORT_5,data_read[5]);
+            OCR0A = data_read[2];
+            OCR0B = data_read[1];
+            OCR1A = data_read[3];
+            OCR1B = data_read[4];
+            OCR2A = data_read[5];
+            OCR2B = data_read[0];
             fsm_state=2;
           } else if(d_reg==KCODE_RES_SKP) { // skip one data
             port_cnt++;
             fsm_state=2;
           } else if (d_reg==KCODE_RES_OFF) { // power OFF
             // power off procedure
-            analogWrite(PWMPORT_0,0);
-            analogWrite(PWMPORT_1,0);
-            analogWrite(PWMPORT_2,0);
-            analogWrite(PWMPORT_3,0);
-            analogWrite(PWMPORT_4,0);
-            analogWrite(PWMPORT_5,0);
+            OCR0A = 0;
+            OCR0B = 0;
+            OCR1A = 0;
+            OCR1B = 0;
+            OCR2A = 0;
+            OCR2B = 0;
             fsm_state=2;
           } else if (d_reg==KCODE_RES_END) { // EXIT
             fsm_state=0;
